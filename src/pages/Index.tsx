b6dbfaf1ab65d53e57aaca1ad85fe4c344c025ba
Sameer Saurav <sameer.sourav@example.com>
2026-03-18 03:05:26 +0530

    removed lenis

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,4 @@
 import { useEffect } from 'react';
-import Lenis from '@studio-freight/lenis';
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
 import Projects from '../components/Projects';
@@ -9,24 +8,9 @@ import Experience from '../components/Experience';
 import Contact from '../components/Contact';
 import CustomCursor from '../components/CustomCursor';
 
+
 const Index = () => {
-  useEffect(() => {
-    const lenis = new Lenis({
-      duration: 1.2,
-      easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      smoothWheel: true,
-    });
-
-    function raf(time: number) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-    requestAnimationFrame(raf);
-
-    return () => {
-      lenis.destroy();
-    };
-  }, []);
+  
 
   return (
     <div className="bg-background text-foreground min-h-screen cursor-none md:cursor-none">
